22a3757771c266b034c21106c25d2754e37adabc
ia8190 <isihaq2013@gmail.com>
2024-11-26 02:12:35 +0000

    testing

--- src/pages/ExhibitorDashboard.jsx
+++ src/pages/ExhibitorDashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ExhibitorDashboard.css";
 
 const ExhibitorDashboard = () => {
-  return <div>Welcome to the Exhibitor Dashboard</div>;
+  return (
+    <div className="exhibitor-dashboard-container">
+      <h1>Welcome to the Exhibitor Dashboarddfeefe</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/exhibitor/register">Register as Exhibitor</Link>
+          </li>
+          <li>
+            <Link to="/exhibitor/books">Manage Your Books</Link>
+          </li>
+          <li>
+            <Link to="/exhibitor/book-stall">Book a Stall</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
-export default ExhibitorDashboard; 
+export default ExhibitorDashboard;
